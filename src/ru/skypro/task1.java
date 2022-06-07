2656b0cddcc08f4e306e913039cbe3ba3ceb46a4
Ivanov Ivan <ivanov93@vk.com>
2022-06-07 15:47:19 +0300

    Домашняя работа №6 Методы сделал 3 задания

--- src/ru/skypro/task1.java
+++ src/ru/skypro/task1.java
@@ -2,6 +2,15 @@ package ru.skypro;
 
 public class task1 {
     public static void main(String[] args) {
+        int year = 2;
+        yearCalculation(year);
+    }
 
+    public static void yearCalculation(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+            System.out.println(year + " является високосным");
+        } else {
+            System.out.println(year + " не является високосным");
+        }
     }
 }
