1ee8ab776bd7fb5fe94a8ac75be73dabbaeb095c
Ivanov Ivan <ivanov93@vk.com>
2022-06-07 15:18:28 +0300

    Домашняя работа №6 Методы

---
+++ src/ru/skypro/lecture.java
@@ -0,0 +1,6 @@
+package ru.skypro;
+
+public class lecture {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/ru/skypro/task2.java
@@ -0,0 +1,7 @@
+package ru.skypro;
+
+public class task2 {
+    public static void main(String[] args) {
+
+    }
+}
