f0497ae812f7f13b723986ddfcde33368bb8209f
Ivanov Ivan <ivanov93@vk.com>
2022-06-07 15:47:30 +0300

    Домашняя работа №6 Методы сделал 3 задания

--- src/ru/skypro/task2.java
+++ src/ru/skypro/task2.java
@@ -2,6 +2,27 @@ package ru.skypro;
 
 public class task2 {
     public static void main(String[] args) {
-
+        int clientOS = 0;
+        int clientDeviceYear = 2016;
+        phone(clientOS, clientDeviceYear);
+    }
+    static void phone(int clientOS, int clientDeviceYear){
+        boolean newIos = clientOS == 0;
+        boolean oldPhone = clientDeviceYear < 2015;
+        boolean newAndroid = clientOS == 1;
+        if (newIos) {
+            if (oldPhone) {
+                System.out.println("Установите облегченную версию приложения для IOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для IOS по ссылке");
+            }
+        }
+        if (newAndroid) {
+            if (oldPhone) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        }
     }
 }
--- src/ru/skypro/task3.java
+++ src/ru/skypro/task3.java
@@ -2,6 +2,22 @@ package ru.skypro;
 
 public class task3 {
     public static void main(String[] args) {
+        int deliveryDistance = 61;
+        int days = estimatedDeliveryTime(deliveryDistance);
+        System.out.println("Потребуется дней: " + days);
+    }
+     public static int estimatedDeliveryTime(int distance){
+        int days = 1;
+        boolean interval1 = distance > 20;
+        boolean interval2 = distance > 60;
 
+        if (interval1) {
+            days++;
+        }
+
+        if (interval2) {
+            days++;
+        }
+        return days;
     }
 }
